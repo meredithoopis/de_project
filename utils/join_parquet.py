6eda3421c7e079d8728d621e5ee93eb248e0001a
hanhnggg <120622330+meredithoopis@users.noreply.github.com>
2024-02-29 17:59:50 +0700

    Update join_parquet.py

--- utils/join_parquet.py
+++ utils/join_parquet.py
@@ -3,8 +3,8 @@ import pyarrow.parquet as pq
 
 if __name__ == "__main__": 
     parquet_file_paths = [
-        "/home/meredith/apps/cap/utils/data/yellow_tripdata_2022-08.parquet", 
-        "/home/meredith/apps/cap/utils/data/yellow_tripdata_2022-09.parquet"
+        "/home/dslab/code/Hn/test/utils/data/yellow_tripdata_2022-08.parquet", 
+        "/home/dslab/code/Hn/test/utils/data/yellow_tripdata_2022-09.parquet"
 ]
     with pq.ParquetWriter("output.parquet", schema = pq.ParquetFile(parquet_file_paths[0]).schema_arrow) as writer: 
         for file in parquet_file_paths: 
